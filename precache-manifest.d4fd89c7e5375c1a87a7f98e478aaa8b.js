474a93dd550a34e8045a529332295a93db461884
pounda <pounda@Pounda-nuc>
2019-01-07 18:06:44 +0100

    Updates

---
+++ precache-manifest.d4fd89c7e5375c1a87a7f98e478aaa8b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c7d801ee586c38afdb9",
+    "url": "/pounda/static/css/main.3faa76b5.chunk.css"
+  },
+  {
+    "revision": "6c7d801ee586c38afdb9",
+    "url": "/pounda/static/js/main.6c7d801e.chunk.js"
+  },
+  {
+    "revision": "5cb9b55e9859431b45ee",
+    "url": "/pounda/static/js/1.5cb9b55e.chunk.js"
+  },
+  {
+    "revision": "1ed8878d0d1a6d0f8dab",
+    "url": "/pounda/static/js/runtime~main.1ed8878d.js"
+  },
+  {
+    "revision": "d8280cd489c94d16fee26d62c74efbbe",
+    "url": "/pounda/index.html"
+  }
+];
